965183d1b19aea38214b5c4017d0f0e2d91d5c57
mferly <mferland@esentire.com>
2019-11-22 20:01:32 -0500

    Handled case where ini_get('session.gc_maxlifetime') might not be available

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ declare(strict_types=1);
 
 try {
     require 'SessionParser.php';
-    echo 'Number of authenticated sessions: '. (new SessionParser)::init((int)ini_get('session.gc_maxlifetime'));
+
+    $sessionGcMaxlifetime = (int) @ini_get('session.gc_maxlifetime');
+    if (empty($sessionGcMaxlifetime)) $sessionGcMaxlifetime = 0;
+
+    echo 'Number of authenticated sessions: '. (new SessionParser)::init($sessionGcMaxlifetime);
 } catch (\Exception $e){
     echo $e->getMessage();
 }
