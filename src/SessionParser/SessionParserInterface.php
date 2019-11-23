a6c2edca2a835f29fbac6c54aebe1bc0c88103be
mferly <mferland1980@gmail.com>
2019-11-23 13:05:11 -0500

    Added Docblocks to interface

--- src/SessionParser/SessionParserInterface.php
+++ src/SessionParser/SessionParserInterface.php
@@ -5,7 +5,12 @@ namespace SessionParser;
 
 interface SessionParserInterface {
 
+    /** Initializes the program. */
     public static function init(string $sessionFolderPath): int;
+
+    /** Iterates through chosen directory */
     public static function fileIterator(): int;
+
+    /** Parses arg looking for needle */
     public static function fileParser(string $sessionString): bool;
 }
