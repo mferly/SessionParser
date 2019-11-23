91da6656ad3a7cb8d419f683c734d41618d732fd
mferly <mferland1980@gmail.com>
2019-11-23 18:39:16 -0500

    Updated Docblock because phpcs was yelling at me

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * Class to parse session files for determining user authenticated sessions.
+ *
+ * PHP version 7.3.11-1+ubuntu18.04.1+deb.sury.org+1
+ *
+ * @category Software
+ * @author   mferly <mferly@example.com>
+ * @license  MIT (https://opensource.org/licenses/MIT)
+ * @file     index.php
+ * @link     https://www.reddit.com/user/mferly
+ */
+
 require './src/autoload.php';
 
 try {
