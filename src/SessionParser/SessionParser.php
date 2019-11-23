a6aac3008ceee97522d5979e8c84b62131bc88dd
mferly <mferland1980@gmail.com>
2019-11-23 11:10:20 -0500

    Updated Docblocks

--- src/SessionParser/SessionParser.php
+++ src/SessionParser/SessionParser.php
@@ -6,77 +6,68 @@ namespace SessionParser;
 /**
  * Class to parse session files for determining user authenticated sessions.
  *
- * @author mferly
- * @website https://www.reddit.com/user/mferly
- * @licence MIT (https://opensource.org/licenses/MIT)
+ * @author mferly (https://www.reddit.com/user/mferly)
+ * @license MIT (https://opensource.org/licenses/MIT)
  */
 final class SessionParser implements SessionParserInterface
 {
 
     /**
-     * @name $fileIterator
-     * @scope private
-     * @type null static
-     * @default null
-     * @description: A placeholder to hold the file iterator so that directory traversal is only
-     *               performed once.
+     * A placeholder to hold the file iterator so that directory traversal is only
+     * performed once.
      */
     private static $fileIterator = null;
 
-    /**
-     * @name $counter
-     * @scope private
-     * @type int static
-     * @description: holds counter of authenticated sessions
-     */
+    /** Holds iterative counter of authenticated sessions */
     private static $counter = 0;
 
     /**
-     * @name $needle
-     * @scope private
-     * @type string static
-     * @description: this is the matching string per the session file to determine
-     *               whether the session is authenticated or not.
-     * @usage:  $_SESSION['auth'] = 1; writes 'auth|b:1' to a session file
+     * This is the matching string per the session file to determine whether the
+     * session is authenticated or not.
+     *
+     * @var string static
+     * @example: $_SESSION['auth'] = 1; writes 'auth|b:1' to a session file
      *              located in the path you've set in static::$sessionFolderPath.
-     *          $_SESSION['auth'] = 0; writes (updates) that session file to
+     *           $_SESSION['auth'] = 0; writes (updates) that session file to
      *              reflect that change: auth|b:0
-     * @replace $_SESSION['auth'] with your program's $_SESSION key/index
+     *           Replace $_SESSION['auth'] with your program's $_SESSION key/index
      *              ie. $_SESSION['authenticated'] = true, etc.
-     *          Update static::$needle to reflect
+     *           Update static::$needle to reflect
+     * @example: array(2) {
+     *      [0]=> string(28) "user|s:6:"mferly";auth|b:0; "
+     *      [1]=> string(34) "user|s:11:"my_username";auth|b:1; "
+     * }
+     * @example:
+     *   auth|b:0 = un-authenticated session
+     *   auth|b:1 = authenticated session
      */
     private static $needle = 'auth|b:1';
 
     /**
-     * @name $sessionFolderPath
-     * @scope private
-     * @type string
-     * @description: path to the folder you want to iterate on
-     * @message: /var/lib/php/sessions is very much likely to be (should be) root access only.
-     *           Executing this script via browser (will not) should not work.
-     *           Execute this script via CLI only.
+     * Path to the directory you want to iterate on.
+     *
+     * @var string
      */
     private static $sessionFolderPath = '/var/lib/php/sessions/';
 
     /**
-     * @name $sessionGcMaxlifetime
-     * @scope private
-     * @type int
-     * @default 3600 seconds (1 hour) This should be overwritten by
-     *                                ini_get('session.gc_maxlifetime')
-     *                                as long as ini_get() is available per ENV.
-     * @description: This is used to check length (in time()) of authenticated session.
-     *               This should be coming from php.ini config automagically as
-     *               requested in index.php
+     * This is used to check length (in time()) of authenticated session.
+     *
+     * Its value should be coming from php.ini config automagically but we assign
+     * a default just in case. We use this to check length (in time()) of
+     * authenticated session.
+     *
+     * @var int
      */
     private static $sessionGcMaxlifetime = 3600;
 
     /**
-     * @name init
-     * @scope public
-     * @type static method
-     * @param $sessionFolderPath string
-     * @return int static::fileIterator()
+     * Initializes the program.
+     *
+     * @param int $sessionGcMaxlifetime Attempt to retrieve 'session.gc_maxlifetime' from php.ini
+     * @param string $sessionFolderPath Path to directory containing session files.
+     * @method static int
+     * @return int
      */
     public static function init(int $sessionGcMaxlifetime, string $sessionFolderPath = ''): int
     {
@@ -87,10 +78,9 @@ final class SessionParser implements SessionParserInterface
     }
 
     /**
-     * @name fileIterator()
-     * @scope public
-     * @type static method
-     * @description: iterates through static::$sessionFolderPath folder
+     * Iterates through $sessionFolderPath and calls fileParser()
+     *
+     * @method static int
      * @return $counter int on success, null on error.
      */
     public static function fileIterator(): int
@@ -134,17 +124,17 @@ final class SessionParser implements SessionParserInterface
     }
 
     /**
-     * @name fileParser
-     * @scope public
-     * @type static method
-     * @param $sessionString string default empty
+     * Parses arg looking for $needle
+     *
+     * @param string $sessionString
      * @example: array(2) {
      *      [0]=> string(28) "user|s:6:"mferly";auth|b:0; "
      *      [1]=> string(34) "user|s:11:"my_username";auth|b:1; "
      * }
-     * @explain:
+     * @example:
      *   auth|b:0 = un-authenticated session
      *   auth|b:1 = authenticated session
+     * @method static bool
      * @return bool
      */
     public static function fileParser(string $sessionString = ''): bool
