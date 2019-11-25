60e938f1009cd181649ffed7e276554ac4f3443a
mferly <mferland1980@gmail.com>
2019-11-25 12:42:07 -0500

    Added tests

---
+++ tests/SessionParser/SessionParserTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace SessionParser;
+
+use PHPUnit\Framework\TestCase;
+
+final class SessionParserTest extends TestCase
+{
+    public function testSessionDirectoryExists(): void
+    {
+        $this->assertDirectoryExists('/var/lib/php/sessions/');
+    }
+
+    public function testSessionDirectoryIsReadable(): void
+    {
+        $this->assertDirectoryIsReadable('/var/lib/php/sessions/');
+    }
+
+    public function testMethodInitReturnsInt(): void
+    {
+        $this->assertIsInt(SessionParser::init());
+    }
+
+    public function testMethodDirectoryIteratorReturnsInt(): void
+    {
+        $this->assertIsInt(SessionParser::directoryIterator());
+    }
+
+    public function testMethodFileParserReturnsBool(): void
+    {
+        $this->assertIsBool(SessionParser::fileParser());
+    }
+}
